0ac77ed13e632f007977cbc1be454af6dcd6925e
rajkumarandekar <rajkumarandekar1144@gmail.com>
2022-12-13 14:36:28 +0530

    d

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,6 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 import axios from "axios";
 import { useLocation } from "react-router";
-
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
